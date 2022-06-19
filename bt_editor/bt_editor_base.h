d710aa8e2b2440a7c287bf109613cc2f02b02468
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2022-06-19 17:01:49 +0900

    Add interpreter mode startup button callback

--- bt_editor/bt_editor_base.h
+++ bt_editor/bt_editor_base.h
@@ -55,7 +55,7 @@ struct  NodeModel
 typedef std::map<QString, NodeModel> NodeModels;
 
 
-enum class GraphicMode { EDITOR, MONITOR, REPLAY };
+enum class GraphicMode { EDITOR, INTERPRETER, MONITOR, REPLAY };
 
 GraphicMode getGraphicModeFromString(const QString& str);
 
